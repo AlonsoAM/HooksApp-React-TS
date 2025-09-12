7cec2a89d3e8f36ddd21c13d5e41ce4e93bed240
Developer <test@example.com>
2025-09-12 14:53:49 -0500

    🎉 feat: Crear nuevos componentes reutilizables para UI
    - Implementa nuevos componentes reutilizables: `Button`, `Card`, `Checkbox`, `Input`
    - Crea utilidades en `utils.ts` para gestionar clases con `tailwind-merge` y `clsx`
    - Configura dependencias de diseño como `lucide-react` y `class-variance-authority`
    - Actualiza `package.json` con librerías necesarias para los nuevos componentes UI
    - Añade archivo `components.json` para gestionar configuraciones de estilo y bibliotecas de íconos

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,13 @@ import "./index.css";
 // import {TrafficLightWithEffect} from "./02-useEffect/TrafficLightWithEffect";
 // import {TrafficLightWithHook} from "./02-useEffect/TrafficLightWithHook.tsx";
 // import {PokemonPage} from "./03-examples/PokemonPage.tsx";
-import {FocusScreen} from "./04-useRef/FocusScreen.tsx";
+// import {FocusScreen} from "./04-useRef/FocusScreen.tsx";
+import {TasksApp} from "@/05-useReducer/TaskApp.tsx";
 // import { HooksApp } from "./HooksApp";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <HooksApp /> */}
-    <FocusScreen />
+    <TasksApp />
   </StrictMode>
 );
