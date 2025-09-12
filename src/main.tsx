6fcd698bbde54fb80f44cf7c72f7de71a35224be
Developer <test@example.com>
2025-09-12 12:48:55 -0500

    🎉 feat: Crear página interactiva para mostrar Pokémon
    - Implementa el componente `PokemonPage` con funciones interactivas usando `useCounter` y `usePokemon`
    - Añade el hook `useCounter` para manejar la lógica del contador
    - Crea el hook `usePokemon` para obtener datos dinámicos desde la PokeAPI
    - Actualiza `main.tsx` para establecer `PokemonPage` como punto de entrada
    - Integra botones de navegación para cambiar entre Pokémon anteriores y siguientes

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 // import { TrafficLight } from "./01-useState/TrafficLight";
 // import {TrafficLightWithEffect} from "./02-useEffect/TrafficLightWithEffect";
-import {TrafficLightWithHook} from "./02-useEffect/TrafficLightWithHook.tsx";
+// import {TrafficLightWithHook} from "./02-useEffect/TrafficLightWithHook.tsx";
+import {PokemonPage} from "./03-examples/PokemonPage.tsx";
 // import { HooksApp } from "./HooksApp";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <HooksApp /> */}
-    <TrafficLightWithHook />
+    <PokemonPage />
   </StrictMode>
 );
